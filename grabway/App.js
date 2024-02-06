b5e2e6a209cee84b0a4e8c42cd93a24817712922
addymistrel <102992848+addymistrel@users.noreply.github.com>
2024-02-06 19:14:53 +0530

    all app structure ready

--- grabway/App.js
+++ grabway/App.js
@@ -1,19 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
     <NavigationContainer>
-      <View>
+      <SafeAreaView>
         <Text>Hello</Text>
-      </View>
+      </SafeAreaView>
       {/* 
         <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
       */}
-
     </NavigationContainer>
   );
 }
@@ -21,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
