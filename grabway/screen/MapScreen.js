b412e7d298edd3b1162644eeb50e3e7684f70417
Abhinav Singh <kittusinghranchi@gmail.com>
2024-02-13 01:01:43 +0530

    mapscreen contents entioned there

--- grabway/screen/MapScreen.js
+++ grabway/screen/MapScreen.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const MapScreen = () => {
     console.log('hello');
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView style={{flexDirection:'column', gap:30, marginTop:50, marginHorizontal:20}}>
+      <Text>
+        Seacrh Destination Box here
+      </Text>
+      <Text>Show Trending Routes from that location by fetching from mongo</Text>
+      <Text>
+        Show favorites in down 
+      </Text>
+    </SafeAreaView>
   )
 }
 
